2fc08082523838240979f074a2f800aac2b341a6
GZC <47302028@qq.com>
2017-02-21 20:21:43 +0800

    4 objects inside resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,81 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+    "name": "Geng ZiCheng",
+    "role": "Front-End Web Developer",
+    "welcomMessage": "Welcom to my resume page",
+    "biopic": "../frontend-nanodegree-resume/images/myavatar.jpg",
+    "contacts": [
+      {
+      "mobile": "138-8058-0559",
+      "email": "gengzicheng@qq.com",
+      "github": "GengZiCheng",
+      "twitter": "@GengZiCheng",
+      "location": "ChengDu,SiChuan,China"
+      }
+    ],
+    "skills":[
+      "High sense of responsibility","honest","teamwork","learning ability"
+    ]
+};
+
+var projects = {
+    "blog-mockup" :[
+      {
+        "title": "blog-mockup",
+        "dates": "Feb 2017",
+        "description": "This was my first project in Udacity class. I learned a lot about HTML and CSS.",
+        "images": "../images/project1.jpg"
+      }
+    ],
+    "animal-card" :[
+      {
+        "title": "animal-card",
+        "dates": "Feb 2017",
+        "description": "I learned more about CSS in this project by using  online CSS tools.",
+        "images": "../images/project2.jpg"
+      }
+    ],
+    "mock-portfolio" :[
+      {
+        "title": "mock-portfolio",
+        "dates": "Feb 2017",
+        "description": "This project is about using different methods to set the framwork of an HTML page.",
+        "images": "../images/project3.JPG"
+      }
+    ]
+};
+
+var education = {
+    "Schools" :[
+      {
+        "name": "Acadia University",
+        "location": "Wolfville,Nova scotia, Canada",
+        "major": ["Business Administration"],
+        "degree": "Bachelor of Business Administration",
+        "dates": "2007-2010",
+        "url": "http://www2.acadiau.ca"
+      }
+    ],
+    "onlineCourses" : [
+      {
+        "school": "Udacity",
+        "title": "Front-End Web Developer",
+        "dates": "2017",
+        "url": "www.udacity.com"
+      }
+    ]
+};
+
+var work = {
+    "jobs": [
+      {
+        "employer": "Tibet Golden Khada Medical Limited Company",
+        "title": "General Manager Assistant",
+        "location": "Tibet, China",
+        "dates": "2011-2013",
+        "description": "I provided assistance in human resource management, sales, finance, property management, public relations, and customer engagement."
+      }
+    ]
+};
